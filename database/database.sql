6292397996efe84fd5fdfac7d0720da24965f88c
Rowan Abisai <107498038+rowanabisaiutp@users.noreply.github.com>
2023-04-29 05:14:20 -0600

    database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,20 +1,14 @@
 CREATE DATABASE tarjeta_presentacion_db;
-
+drop database tarjeta_presentacion_db;
 USE tarjeta_presentacion_db;
 
-CREATE TABLE cliente(
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255),
-    especialidad VARCHAR(255),
-    num_tel INT(11) DEFAULT NULL,
-    imagen VARCHAR(255),
-    municipio VARCHAR(255),
-    estado VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-
-RENAME TABLE cliente to clientes;
-
-DESCRIBE clientes;
-
-SELECT * FROM clientes;
+CREATE TABLE clientes(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  especialidad VARCHAR(50),
+  num_tel NUMERIC(10,0),
+  imagen VARCHAR(255),
+  municipio VARCHAR(50),
+  estado VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
